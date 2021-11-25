b8f2080e0a62e2e3fb27a23dba2cc8a2ba1e7255
Nicholas Vollmer <iarchivedmywholelife@gmail.com>
2021-11-25 14:58:31 -0500

    Satisfy package-lint

--- autoload-test.el
+++ autoload-test.el
@@ -1,13 +1,23 @@
 ;;; autoload-test.el --- Repro for a bug with autoloads  -*- lexical-binding: t; -*-
 
+;; Author: Nicholas Vollmer <progfolio@protonmail.com>
+;; URL: https://github.com/progfolio/autoload-test
+;; Created: 2021-11-25
+;; Keywords: tools
+;; Package-Requires: ((emacs "25.1"))
+;; Version: 0.0.0
 ;; Copyright (C) 2021
 
-;; Author:
-;; Keywords:
+;;; Commentary:
+;; A test for autoload encoding with straight.el
+
+
+;;; Code:
 
 ;;;###autoload
 (defun autoload-test ()
-  "Тест кирилица"
+;;  "Тест кирилица"
+  "Test."
   nil)
 
 (provide 'autoload-test)
